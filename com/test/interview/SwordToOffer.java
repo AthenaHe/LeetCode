d54ca44188990c7c047bda0604c4ee6d84a6df6d
AthenaHe <AthenaHe126@gmail.com>
2020-02-18 23:18:10 +0800

    add: 面试题55 - I. 二叉树的深度 面试题27. 二叉树的镜像 面试题32 - I. 从上到下打印二叉树 面试题32 - II. 从上到下打印二叉树 II

--- com/test/interview/SwordToOffer.java
+++ com/test/interview/SwordToOffer.java
@@ -3,8 +3,69 @@ package com.test.interview;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
 
 public class SwordToOffer {
+//树结点定义
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+/*
+ * 面试题27. 二叉树的镜像
+ */
+public TreeNode mirrorTree(TreeNode root) {
+   if (root==null) return null;
+   TreeNode leftnode = mirrorTree(root.right);
+   TreeNode rightnode = mirrorTree(root.left);
+   root.left = leftnode;
+   root.right = rightnode;
+   return root;
+}
+/*
+ * 面试题32 - I. 从上到下打印二叉树
+ */
+public int[] levelOrder1(TreeNode root) {
+    List<Integer> list = new LinkedList<>();
+    Queue<TreeNode> queue = new LinkedList<>();
+    if (root==null)  return new int[0];
+    queue.offer(root);
+    while(!queue.isEmpty()) {
+    	TreeNode node = queue.poll();
+    	list.add(node.val);
+    	if(node.left!=null) queue.offer(node.left);
+    	if (node.right!=null) queue.offer(node.right);	
+    }
+    int[] res =new int[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+		res[i] = list.get(i);
+	}
+    return res;
+}
+//面试题32 - II. 从上到下打印二叉树 II
+public List<List<Integer>> levelOrder2(TreeNode root) {
+	List<List<Integer>> lists = new LinkedList<>();
+	Queue<TreeNode> queue = new LinkedList<>();
+	queue.offer(root);
+	while(!queue.isEmpty()) {
+		int count = queue.size();
+		TreeNode node = queue.poll();
+		List<Integer> list = new LinkedList<>();
+		while(count>0) {
+			list.add(node.val);
+			if(node.left!=null) queue.offer(node.left);
+			if(node.right!=null) queue.offer(node.right);
+			count--;
+		}
+		lists.add(list);
+	}
+	return lists;  
+}
 /*
  * 面试题49. 丑数
  */
@@ -21,6 +82,13 @@ public int nthUglyNumber(int n) {
 	}
 	return ugly[n-1];      
     }
+/*
+ * 面试题55 - I. 二叉树的深度
+ */
+public int maxDepth(TreeNode root) {
+    if (root==null) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+}
 /*
  * 面试题56 - I. 数组中数字出现的次数
  */
@@ -69,8 +137,8 @@ public int sumNums(int n) {
 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
